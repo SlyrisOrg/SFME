a494d73e68e058d54eb050ccc5aaf20bf91ccb19
sztergbaum roman <roman.sztergbaum@epitech.eu>
2018-02-21 23:26:09 +0100

    fix coding style

--- SFME/mediator/SFME/mediator/event_manager.hpp
+++ SFME/mediator/SFME/mediator/event_manager.hpp
@@ -17,15 +17,18 @@ namespace sfme::mediator
     {
     public:
         template <typename TEvent, typename TReceiver>
-		void subscribe(TReceiver &receiver) noexcept
-		{
-			static_assert(details::is_mediator_event<TEvent>, "The template parameter must be base of BaseEvent");
-			const details::EventTypeID eventTypeID = details::getEventTypeID<TEvent>();
-			const BaseReceiver &baseReceiver = receiver;
-			_receiversRegistry.insert(std::make_pair(eventTypeID, ReceiverData{&baseReceiver,
-				std::make_unique<details::EventCallbackWrapper<TEvent>>([&receiver](const auto &ev) noexcept {
-				receiver.receive(ev);
-			})}));
+        void subscribe(TReceiver &receiver) noexcept
+        {
+            static_assert(details::is_mediator_event<TEvent>, "The template parameter must be base of BaseEvent");
+            const details::EventTypeID eventTypeID = details::getEventTypeID<TEvent>();
+            const BaseReceiver &baseReceiver = receiver;
+            _receiversRegistry.insert(std::make_pair(eventTypeID,
+                                                     ReceiverData{&baseReceiver,
+                                                                  std::make_unique<details::EventCallbackWrapper<TEvent>>(
+                                                                      [&receiver](
+                                                                          const auto &ev) noexcept {
+                                                                          receiver.receive(ev);
+                                                                      })}));
         };
 
         template <typename TEvent, typename ... Args>
@@ -34,29 +37,28 @@ namespace sfme::mediator
             static_assert(details::is_mediator_event<TEvent>, "The template parameter must be base of BaseEvent");
             TEvent event(std::forward<Args>(args)...);
             const details::EventTypeID eventTypeID = details::getEventTypeID<TEvent>();
-	        auto&& receivers = _receiversRegistry.equal_range(eventTypeID);
-			std::for_each(receivers.first, receivers.second, [&event](const auto& receiverEntry) noexcept
-			{
-				receiverEntry.second.callback->operator()(&event);
-			});
+            auto &&receivers = _receiversRegistry.equal_range(eventTypeID);
+            std::for_each(receivers.first, receivers.second, [&event](const auto &receiverEntry) noexcept {
+                receiverEntry.second.callback->operator()(&event);
+            });
         };
 
         bool isRegister(const BaseReceiver &receiver) const noexcept
         {
-			return std::any_of(begin(_receiversRegistry), end(_receiversRegistry), [&receiver](auto const& receiverEntry)
-			{
-				return receiverEntry.second.receiver == &receiver;
-			});
+            return std::any_of(begin(_receiversRegistry), end(_receiversRegistry),
+                               [&receiver](auto const &receiverEntry) {
+                                   return receiverEntry.second.receiver == &receiver;
+                               });
         }
 
     private:
         using CallbackPtr = std::unique_ptr<details::BaseEventCallbackWrapper>;
-		struct ReceiverData
-    	{
-			const BaseReceiver *receiver;
-			CallbackPtr callback;
-		};
-		using ReceiversMap = std::unordered_multimap<details::EventTypeID, ReceiverData>;
+        struct ReceiverData
+        {
+            const BaseReceiver *receiver;
+            CallbackPtr callback;
+        };
+        using ReceiversMap = std::unordered_multimap<details::EventTypeID, ReceiverData>;
         ReceiversMap _receiversRegistry;
     };
 }
